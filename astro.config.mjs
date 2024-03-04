4b224d5f18c8971bf5bb8d1b1f80cb8a9da73da2
Kevin Smith Jimenez Garro <kevinedk33@gmail.com>
2024-03-04 18:35:05 -0500

    3
    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  // base: "/baum2024/public_html/",
+  base: "/baum2024/public_html",
   integrations: [tailwind(), react()],
 });
