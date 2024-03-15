d6d18867e15cc550cff8f52420ed4ac129fd0395
Kevin Smith Jimenez Garro <kevinedk33@gmail.com>
2024-03-15 13:21:13 -0500

    final
    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/",
+  base: "/baumfestival",
   integrations: [tailwind(), react()],
 });
