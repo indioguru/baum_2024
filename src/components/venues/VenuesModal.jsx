f9f135564b54eadd7e04939c6d8027c89052efb6
Kevin Smith Jimenez Garro <kevinedk33@gmail.com>
2024-05-11 13:10:08 -0500

    Sabado sabado sabadoi

--- src/components/venues/VenuesModal.jsx
+++ src/components/venues/VenuesModal.jsx
@@ -16,8 +16,8 @@ const VenuesModal = ({ data }) => {
           {currentModal === index && (
             <>
               <p className="mt-[13px] text-[14px] text-white pp-book lg:w-[70%] lg:text-[18px] ">{item?.Texto}</p>
-              <div className="button_two text-end mt-[22px]">
-                <a href={item?.Link} target="_blank" rel="noopener noreferrer">
+              <div className="button_two text-end mt-[22px] ver_mas_resident_fabric">
+                <a className="ver_mas_resident_fabric" href={item?.Link} target="_blank" rel="noopener noreferrer ">
                   VER MÁS
                 </a>
               </div>
